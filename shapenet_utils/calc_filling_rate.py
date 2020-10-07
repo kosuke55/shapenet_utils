404819b1a68c05b038f82cbc127449bff8ce5c05
kosuke55 <kosuke.tnp@gmail.com>
2020-10-08 00:56:21 +0900

    Update

---
+++ shapenet_utils/calc_filling_rate.py
@@ -0,0 +1,108 @@
+import json
+import os.path as osp
+import sys
+from pathlib import Path
+
+import numpy as np
+import matplotlib.pyplot as plt
+import trimesh
+from tqdm import tqdm
+
+from shapenet_synset_dict import synset_to_label
+
+
+def save_json(save_file, data, sort_key=True):
+    """Save json
+
+    Parameters
+    ----------
+    save_file : str
+    data : list or dict
+    sort_key : bool, optional
+        by default True
+    """
+    with open(save_file, 'w') as f:
+        json.dump(data, f, ensure_ascii=False,
+                  indent=4, sort_keys=sort_key, separators=(',', ': '))
+
+
+def load_json(file):
+    """Load json file
+
+    Parameters
+    ----------
+    file : str
+        input json file
+
+    Returns
+    -------
+    data : dict
+    """
+    with open(file, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+def make_graph(data, method):
+    synset = []
+    value = []
+    for d in [[k, v] for k, v in data.items()]:
+        synset.append(d[0])
+        value_list = []
+        for _d in [[k, v] for k, v in d[1].items()]:
+            value_list.append(_d[1])
+        value.append(np.mean(value_list))
+
+    label = [synset_to_label[s] for s in synset]
+
+    fig = plt.figure()
+    plt.bar(label, value, width=0.5)
+    plt.title('filling rate %s' % method.replace('_', ' '))
+    plt.xlabel('category')
+    plt.xticks(rotation=90)
+    plt.ylabel('filling rate')
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig('filling_rate_%s.png' % method, format='png', dpi=300)
+
+
+shapenet_dir = '/media/kosuke/SANDISK/meshdata/ShapeNetCore.v2'
+paths = sorted(list(Path(shapenet_dir).glob(
+    '*/*/models/model_normalized.obj')))
+
+data = {}
+data_box = {}
+prev_category = None
+
+for path in tqdm(paths):
+    category = str(path.parent.parent.parent.name)
+    # if prev_category is not None and category == prev_category:
+    #     if len(data[category]) == 3:
+    #         continue
+    if category not in data:
+        data[category] = {}
+        data_box[category] = {}
+
+    prev_category = category
+    mesh = trimesh.load(
+        str(path), skip_materials=True,
+        skip_texture=True, force='mesh')
+
+    voxel_path = path.parent / 'model_normalized.solid.binvox'
+    if voxel_path.exists():
+        voxel = trimesh.load(str(voxel_path))
+    else:
+        voxel = mesh.voxelized(0.01)
+
+    filling_rate_box = voxel.volume / mesh.bounding_box_oriented.volume
+    filling_rate_convex_hull = voxel.volume / mesh.convex_hull.volume
+    print(filling_rate_convex_hull)
+    data_id = str(path.parent.parent.name)
+    data_box[category][data_id] = filling_rate_box
+    data[category][data_id] = filling_rate_convex_hull
+
+save_json('shapenet_filling_rate_convex_hull.json', data)
+save_json('shapenet_filling_rate_bounding_box.json', data_box)
+
+make_graph(data, 'convex_hull')
+make_graph(data_box, 'bounding_box')
